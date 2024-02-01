98d0428ae088ab95c582badb3888d8729d6a6eb1
GowthamG127 <gowthamsandhi127@gmail.com>
2024-02-01 13:30:12 +0530

    All modules are added

--- src/com/atm/function/AtmExecution.java
+++ src/com/atm/function/AtmExecution.java
@@ -3,6 +3,7 @@ package com.atm.function;
 public class AtmExecution extends FunctionsOfAnAtm{
 	public static void main(String[] args) {
 		AtmExecution atm = new AtmExecution();
+		// welcome message method has been called
 		atm.welcomeMessage();
 	}
 }
